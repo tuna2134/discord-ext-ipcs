cbe5bae12bea52760d57d05e345e814d8d898085
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-05-11 22:36:10 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+def _requires_from_file(filename):
+    return open(filename, encoding="utf8").read().splitlines()
+
+extras_require = {
+    "speed": [
+        "orjson"
+    ]
+}
+
+setuptools.setup(
+    name="discord-ext-ipc",
+    version="0.0.1a",
+    author="DMS",
+    author_email="masato190411@gmail.com",
+    description="Discord ipc client.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tuna2134/discord-ext-ipc",
+    install_requires=_requires_from_file('requirements.txt'),
+    extras_require=extras_require,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
