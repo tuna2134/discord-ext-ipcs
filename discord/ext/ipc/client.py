b4284e92645c2d8fcf92a7da93bcc2d7d612af11
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-05-11 21:12:48 +0900

    Update client.py

--- discord/ext/ipc/client.py
+++ discord/ext/ipc/client.py
@@ -18,6 +18,7 @@ class Client:
         self.loop = loop
         self.ws: WebSocketClientProtocol = None
         self.events: list = []
+        self.uri: str = None
         
     async def __aenter__(self):
         if self.loop is None:
@@ -27,17 +28,36 @@ class Client:
         await self.close()
         self.loop.close()
         
-    async def connect(self, host: str, port: int=80, *, scheme: str="ws"):
-        self.host, self.port, self.scheme = host, port, scheme
+    async def connect(self, uri: str) -> None:
+        """Connect to ipc server
+        
+        Args:
+            uri (str): URI
+            
+        Examples:
+            await ipc_client.connect("ws://localhost/ipc")
+        
+        Raises:
+            ConnectionError: If you already connect, it will be raise.
+        """
+        self.uri = uri
         if self.ws is not None:
             raise ConnectionError("Already connected")
-        self.ws = await connect("{0}://{1}:{2}".format(scheme, host, port))
+        self.ws = await connect(uri)
         self.client.dispatch("ipc_connect")
         await self.login()
         while self.ws.open:
             await self.recv()
     
-    async def close(self):
+    async def close(self) -> None:
+        """Close from ipc server
+        
+        Raises:
+            ConnectionError: If you already close, it will be raise.
+            
+        Examples:
+            await ipc_client.close()
+        """
         if self.ws is not None:
             if not self.ws.closed:
                 await self.ws.close()
@@ -46,15 +66,34 @@ class Client:
         else:
             raise ConnectionError("Already closed")
             
-    async def reconnect(self):
+    async def reconnect(self) -> None:
+        """Reconnect from ipc server
+        
+        Examples:
+            await ipc_client.reconnect()
+        """
         await self.close()
         await asyncio.sleep(0.5)
-        await self.connect(self.host, self.port, scheme=self.scheme)
+        await self.connect(self.uri)
             
     def login(self):
+        """Login to ipc server
+        
+        Examples:
+            await ipc_client.login()
+        """
         return self.request("login")
             
-    async def request(self, eventtype: str, data: dict={}):
+    async def request(self, eventtype: str, data: dict={}) -> None:
+        """Send something to ipc server
+        
+        Args:
+            eventtype (str): event type
+            data (dict): The data you want to send.
+            
+        Examples:
+            await ipc_client.request("hello", {"message": "What your name"})
+        """
         payload = {
             "type": eventtype,
             "data": data
@@ -62,6 +101,16 @@ class Client:
         await self.ws.send(dumps(payload))
         
     def listen(self, eventtype: str):
+        """Catch data from ipc server
+        
+        Args:
+            eventtype (str): Event type.
+            
+        Examples:
+            @ipc_client.listen("hello")
+            async def hello(response):
+                await self.request("hello", {"message": "My name is Satoshi"})
+        """
         def decorator(func):
             if eventtype in self.events:
                 self.events[eventtype].append(func)
@@ -70,7 +119,7 @@ class Client:
             return func
         return decorator
     
-    async def recv(self):
+    async def recv(self) -> None:
         data = loads(await self.ws.recv())
         
         if data["type"] == "close":
@@ -86,6 +135,15 @@ class Client:
             )
 
 class ResponseItem:
+    """Response data
+    
+    Args:
+        data (dict): Some kind of data.
+        
+    Examples:
+        response = ResponseItem({"hello": "world", "message": "What your name?"})
+        print(response.hello)
+    """
     def __init__(self, data: dict):
         for name, value in data.items():
             setattr(self, name, value)
