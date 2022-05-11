2229d87221b3339b430440f938b2336a0f6e2be7
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-05-11 20:54:27 +0900

    Update client.py

--- discord/ext/ipc/client.py
+++ discord/ext/ipc/client.py
@@ -1,3 +1,5 @@
+# discord.ext.ipc - client
+from websockets.client import WebSocketClientProtocol
 from .errors import ConnectionError
 from websockets import connect
 from discord import Client
@@ -14,7 +16,7 @@ class Client:
         self.client = client
         self.secret_key = secret_key
         self.loop = loop
-        self.ws = None
+        self.ws: WebSocketClientProtocol = None
         self.events: list = []
         
     async def __aenter__(self):
@@ -22,23 +24,37 @@ class Client:
             self.loop = asyncio.get_running_loop()
         
     async def __aexit__(self, *args):
-        await self.ws.close()
+        await self.close()
         self.loop.close()
         
     async def connect(self, host: str, port: int=80, *, scheme: str="ws"):
+        self.host, self.port, self.scheme = host, port, scheme
         if self.ws is not None:
             raise ConnectionError("Already connected")
         self.ws = await connect("{0}://{1}:{2}".format(scheme, host, port))
         self.client.dispatch("ipc_connect")
-        while not self.ws.closed:
+        await self.login()
+        while self.ws.open:
             await self.recv()
     
     async def close(self):
         if self.ws is not None:
-            await self.ws.close()
-            self.client.dispatch("ipc_close")
+            if not self.ws.closed:
+                await self.ws.close()
+                self.client.dispatch("ipc_close")
+            self.ws = None
+        else:
+            raise ConnectionError("Already closed")
             
-    async def request(self, eventtype: str, data: dict):
+    async def reconnect(self):
+        await self.close()
+        await asyncio.sleep(0.5)
+        await self.connect(self.host, self.port, scheme=self.scheme)
+            
+    def login(self):
+        return self.request("login")
+            
+    async def request(self, eventtype: str, data: dict={}):
         payload = {
             "type": eventtype,
             "data": data
@@ -56,6 +72,10 @@ class Client:
     
     async def recv(self):
         data = loads(await self.ws.recv())
+        
+        if data["type"] == "close":
+            self.client.dispatch("ipc_close")
+
         if data["type"] in self.events:
             self.loop.create_task(
                 self.events[data["type"]](
