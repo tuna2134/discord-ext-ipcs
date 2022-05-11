1c8120c1eb001fa349f025a3589267fa0938e479
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-05-11 22:51:30 +0900

    Create items.py

---
+++ discord/ext/ipc/items.py
@@ -0,0 +1,13 @@
+class ResponseItem:
+    """Response data
+    
+    Args:
+        data (dict): Some kind of data.
+        
+    Examples:
+        response = ResponseItem({"hello": "world", "message": "What your name?"})
+        print(response.hello)
+    """
+    def __init__(self, data: dict):
+        for name, value in data.items():
+            setattr(self, name, value)
