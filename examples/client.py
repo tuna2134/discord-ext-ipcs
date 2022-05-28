6d20595c960be75a2184901d90e90b84c6d0bfb1
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-05-28 12:01:20 +0900

    Update client.py

--- examples/client.py
+++ examples/client.py
@@ -6,11 +6,11 @@ from discord.ext import ipc
 class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.ipc_client = ipc.Client(client, "Test")
+        self.ipc = ipc.Client(client, "Test")
         
     async def setup_hook(self):
-        async with ipc_client:
-            await ipc_client.connect("ws://localhost:8080")
+        async with self.ipc:
+            await self.ipc.connect("ws://localhost:8080")
     
     async def on_ipc_connect(self, r):
         print("Connected")
